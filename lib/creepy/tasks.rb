09d879c45e5d35088d8d76de329d685f33245354
Kazuya Takeshima <mail@mitukiii.jp>
2012-01-14 00:38:34 +0900

    2 回目の rm -rf * =͟͟͞͞( ∩ω∩ ) _`’;.;(,`(.　､,）*)”.:';"_ ﾎﾞﾝｯ

--- lib/creepy/tasks.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-module Creepy
-  class Tasks < Mapper
-    Dir[File.dirname(__FILE__) + '/tasks/{task}.rb'].each {|f| require f}
-    Dir[Dir.pwd + '/tasks/*.rb'].each {|f| require f}
-  end
-end
