d57382077acece780c3b1f9a2d1059e910bf87fb
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-04 17:09:11 +0900

    keyword の hook に自分のものは除くオプション追加 (´･ω･`๑)

--- lib/creepy/hooks/keyword.rb
+++ lib/creepy/hooks/keyword.rb
@@ -3,14 +3,15 @@
 module Creepy
   module Hooks
     class Keyword
-      attr_accessor :include, :exclude, :hooks, :formatter, :notifies
+      attr_accessor :include, :exclude, :hooks, :formatter, :notifies, :ignore_self
 
       def initialize(options = {}, &block)
-        @include   = options.fetch :include,   []
-        @exclude   = options.fetch :exclude,   []
-        @hooks     = options.fetch :hooks,     []
-        @formatter = options.fetch :formatter, Formatter.default
-        @notifies  = options.fetch :notifies,  []
+        @include     = options.fetch :include,     []
+        @exclude     = options.fetch :exclude,     []
+        @hooks       = options.fetch :hooks,       []
+        @formatter   = options.fetch :formatter,   Formatter.default
+        @notifies    = options.fetch :notifies,    []
+        @ignore_self = options.fetch :ignore_self, false
         yield self if block_given?
       end
 
@@ -19,9 +20,24 @@ module Creepy
         return unless @include.flatten.any? {|k| status.text.match(k)}
         keyword = $&.to_s
         return if @exclude.flatten.any? {|k| status.text.match(k)}
+        return if ignore_self? && yourself?(status.user.screen_name)
         @hooks.each {|h| h.call(keyword, status)}
         title, message, options = @formatter.call(keyword, status)
         @notifies.each {|n| n.call(title, message, options)}
+      end
+
+      def ignore_self!
+        @ignore_self = true
+      end
+      alias_method :ignore_self?, :ignore_self
+
+      private
+      def yourself?(screen_name)
+        credentials.screen_name == screen_name
+      end
+
+      def credentials
+        Creepy.config.twitter.credentials
       end
 
       module Formatter
