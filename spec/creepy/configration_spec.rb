36dbbe953fc39959f9ad7c8d97978b4fb027f21a
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-04 15:55:16 +0900

    Configuration の spec 書いた ( ◠‿◠)

--- spec/creepy/configration_spec.rb
+++ spec/creepy/configration_spec.rb
@@ -3,11 +3,20 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Creepy::Configuration do
+  before(:each) do
+    configatron.reset!
+  end
+
   describe :config do
     it 'Configuration::Store のインスタンスであること' do
       Creepy.config.should be_a Configatron::Store
     end
 
+    it '値が設定出来ること' do
+      Creepy.config.hoge.should be_nil
+      Creepy.config.hoge = :hoge
+      Creepy.config.hoge.should == :hoge
+    end
   end
 
   describe :configure do
@@ -15,6 +24,14 @@ describe Creepy::Configuration do
       Creepy.configure do |config|
         config.should == Creepy.config
       end
+    end
+
+    it 'ブロックで値が設定出来ること' do
+      Creepy.config.hoge.should be_nil
+      Creepy.configure do |config|
+        config.hoge = :hoge
+      end
+      Creepy.config.hoge.should == :hoge
     end
   end
 end
