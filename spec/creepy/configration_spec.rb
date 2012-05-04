03a62e05e44461d537bd49cb92eb5795a0e0ddfd
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-04 16:14:10 +0900

    register_default の spec 書いた ('ω' っ )３

--- spec/creepy/configration_spec.rb
+++ spec/creepy/configration_spec.rb
@@ -3,7 +3,7 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Creepy::Configuration do
-  before(:each) do
+  after(:each) do
     configatron.reset!
   end
 
@@ -32,6 +32,31 @@ describe Creepy::Configuration do
         config.hoge = :hoge
       end
       Creepy.config.hoge.should == :hoge
+    end
+  end
+
+  describe :register_default do
+    it 'ブロックに Creepy.config が渡されること' do
+      Creepy.register_default do |config|
+        config.should == Creepy.config
+      end
+    end
+
+    it 'ブロックで値が設定出来ること' do
+      Creepy.config.hoge.should be_nil
+      Creepy.register_default do |config|
+        config.hoge = :hoge
+      end
+      Creepy.config.hoge.should == :hoge
+    end
+
+    it 'restore_defaults 時に default が評価されること' do
+      dummy = lambda {}
+      dummy.should_receive(:call).twice
+      Creepy.register_default do
+        dummy.call
+      end
+      Creepy.reload_config!
     end
   end
 end
